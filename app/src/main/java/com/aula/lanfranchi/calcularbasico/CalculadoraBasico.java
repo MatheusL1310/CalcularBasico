cd70f302f61bfac455a26677e67fde24a3f1f911
MatheusL1310 <matheus131097@gmail.com>
2017-05-11 12:13:13 -0300

    first commit

---
+++ app/src/main/java/com/aula/lanfranchi/calcularbasico/CalculadoraBasico.java
@@ -0,0 +1,13 @@
+package com.aula.lanfranchi.calcularbasico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculadoraBasico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora_basico);
+    }
+}
